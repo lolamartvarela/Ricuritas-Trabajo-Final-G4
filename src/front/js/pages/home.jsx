25f2033871270f9f00f65595fc263a70109fdf9e
MatiasRodriguez09 <rodriguezlezcanomatias@gmail.com>
2023-02-06 17:31:17 +0000

    Footer y navbar

---
+++ src/front/js/pages/home.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="text-center mt-5">
+      {" "}
+      <h1> Hello Rigo !! </h1>{" "}
+     {" "}
+      <div className="alert alert-info">
+        {" "}
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}{" "}
+      </div>{" "}
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="">Read documentation </a>{" "}
+      </p>{" "}
+    </div>
+  );
+};
