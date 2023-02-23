cb4eb914ff412be5743e9cbf1d03a9f39e9a3042
lolamartvarela <luciamvarela@outlook.com>
2023-02-23 20:18:32 +0000

    cambios en index.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,13 +2,12 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import Login from "./pages/login"
-import Dashboard from "./pages/dashboard"
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 import Admin_Login from "./pages/admin-login";
-import {Register} from "./pages/register"
+import {Register} from "./pages/register";
 
 import {Home} from "./pages/home.jsx";
-import {Demo} from "./pages/demo";
 import {Single} from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -25,13 +24,11 @@ import Carrusel from "./pages/Tips.jsx";
 import Perfil from "./pages/perfil.jsx";
 import CarritoCompras from "./pages/carrito";
 
-
 // create your first component
 const Layout = () => {
     // the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
-
 
     return (
         <div>
@@ -55,8 +52,6 @@ const Layout = () => {
                             element={<Perfil/>}/>
                         <Route element={<Home/>}
                             path="/"/>
-                        <Route element={<Demo/>}
-                            path="/demo"/>
                         <Route element={<Login/>}
                             path="/login"/>
                         <Route element={<Admin_Login/>}
@@ -79,7 +74,6 @@ const Layout = () => {
                 {" "} </BrowserRouter>
             {" "} </div>
     );
-
 };
 
 export default injectContext(Layout);
